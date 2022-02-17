65f5e8ac14dbed47c8765f356a3d8831565361d1
Sergei Surovtsev <ssurovsev@gmail.com>
2022-02-17 19:00:10 +0300

    rks data

--- apis/rks.py
+++ apis/rks.py
@@ -1,22 +1,29 @@
 import requests
 from pyquery import PyQuery as pq
 from progress.bar import Bar
+import json
 
 
 def parse_rks(page):
     url = "https://reestr.rublacklist.net/?page={}".format(page)
     result = pq(requests.get(url).text)
+    links = []
     for record in result("table tbody tr"):
         date = pq(pq(record)("td")[1]).text()
         url = pq(pq(record)("td")[2]).text()
         dep = pq(pq(record)("td")[4]).text()
-        yield (date, url, dep)
+        links.append((date, url, dep))
+    return links
 
 def fetch_rks():
     links = []
-    for page in Bar('BlocksByTimeRKS', check_tty=False).iter(range(1, 22000)):
-        new_list = list(parse_rks(page))
+    for page in Bar('BlocksByTimeRKS', check_tty=False).iter(range(7236+2698, 22000)):
+        new_list = parse_rks(page)
         links += new_list
+        
+        with open('data/03-rks.json', 'w') as fp:
+            json.dump(links, fp,  indent=4)
+        
         if len(new_list) < 50: continue 
         page += 1
     return links
